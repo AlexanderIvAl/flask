f1e1bf916087443f05b81e7e41533c2c00ef76ff
Alexander <ivalester@mail.ru>
2022-03-09 12:46:03 +0300

    add html

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 from vsearch import search4letters
 
@@ -7,18 +8,20 @@ app = Flask(__name__)
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    results = str(search4letters(phrase, letters))
     title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
     return render_template('results.html',
-                            the_title=title,
-                            the_phrase=phrase,
-                            the_letters=letters,
-                            the_results=results)
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
-    
+    return render_template('entry.html',
+                           the_title='Welcome to search4letters on the web!')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
